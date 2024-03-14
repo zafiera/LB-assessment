dbe8164cd840ab89615c20697285f04ae85f5a5f
Zafiera <zafieradavids1999@gmail.com>
2024-03-14 10:45:53 +0200

    create components

--- lba/src/app/app.module.ts
+++ lba/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { DemoFormComponent } from './demo-form/demo-form.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    DemoFormComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
---
+++ lba/src/app/demo-form/demo-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form',
+  templateUrl: './demo-form.component.html',
+  styleUrl: './demo-form.component.scss'
+})
+export class DemoFormComponent {
+
+}
